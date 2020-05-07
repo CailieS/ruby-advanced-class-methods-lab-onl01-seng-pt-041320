4dc2e94c652984d024cc3f0c725d0d9188476646
Cailie Skelton <cskelton707@gmail.com>
2020-05-07 21:56:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,6 +41,12 @@ class Song
           self.create_by_name(title)
     end
   end
+  
+  def self.alphabetical
+    sorted = self.all.sort_by {|song| song.name}
+    sorted
+  end
+  
   def self.new_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
